b4465dd7614b551cfdfc310b30286e980f651eb5
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-08-02 19:49:09 -0700

    deleted public off github

--- public/component---src-pages-404-js-3554ccd6950c64e4757f.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktouch_of_elegance=self.webpackChunktouch_of_elegance||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var u=n(7294),l=n(7539),a=n(3751);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-3554ccd6950c64e4757f.js.map
